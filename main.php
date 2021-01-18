62e8ab2756b25bdd4209ae88e2211fce67f9bb4b
imitatortop <77597830+imitatortop@users.noreply.github.com>
2021-01-18 23:41:13 +0800

    Update main.php

--- main.php
+++ main.php
@@ -10,7 +10,7 @@
      */
     $cowtransfer = new cowtransfer(); 
 
-    //登录 并 查询 存储大小
+    //登录 并 查询 存储大小          账户                 密码
     // echo $cowtransfer->Login("430272991@qq.com","12345678")->info();  
     $cowtransfer->Login("430272991@qq.com","12345678");
     echo $cowtransfer->info();

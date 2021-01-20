cc165269e434a4f55205b86fa0d162ff0830b8a5
imitatortop <77597830+imitatortop@users.noreply.github.com>
2021-01-20 18:57:29 +0800

    Update main.php

--- main.php
+++ main.php
@@ -1,36 +1,32 @@
 <?php
 /**
- *      两种方式    (new 实例)和  (静态 cowtransfer:: )
+ *      需要登录的模块
+ *      存储大小:   cowtransfer::info()     
+ *      查看根目录: cowtransfer::TableOfContents()  
+ * 
+ *      无需登录的模块
+ *      下载:      cowtransfer::Download_file('guid') 
  */
-    include './cowtransfer_API.php';
-    use cowtransfer_API\cowtransfer;
-    
-    /**
-     *  new 实例
-     */
-    $cowtransfer = new cowtransfer(); 
 
-    //登录 并 查询 存储大小          账户                 密码
-    // echo $cowtransfer->Login("430272991@qq.com","12345678")->info();  
-    $cowtransfer->Login("430272991@qq.com","12345678");
-    echo $cowtransfer->info();
+include "./cowtransfer_API.php";
+use cowtransfer_API\cowtransfer;
 
-    echo '<hr>';
-
-    //查看 目录 文件
-    // 参数为空：为 根目录 ；参数为目录guid
-    echo $cowtransfer->TableOfContents();   //根目录
-    // echo $cowtransfer->TableOfContents(guid);
-
-    // echo '<hr>';
-
-    //下载文件 [ 暂时有问题 命名空间 无法获取 登录参数 ]
-    echo $cowtransfer->Download_file("761df23e-b3da-4a75-b1e4-43f485874978");
-
-/******************************************************************************************* */
 /**
- *      静态方法 cowtransfer::
+ *      new 实例
  */
-    cowtransfer::Download_file("761df23e-b3da-4a75-b1e4-43f485874978");
+    $cowtransfer = new cowtransfer();
+    $cowtransfer->Login("433972421@qq.com","12345678");
+    $cowtransfer->info();
+    $cowtransfer->TableOfContents();
+    $cowtransfer->Download_file("6d346560-7e40-43ac-b327-cb83a781e7a7");
+
+
+/**
+ *      静态  cowtransfer::
+ */
+    cowtransfer::Login("430972921@qq.com","imitator");
+    cowtransfer::info();
+    cowtransfer::TableOfContents();
+    cowtransfer::Download_file("6d346560-7e40-43ac-b327-cb83a781e7a7");
 
 ?>
